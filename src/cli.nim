09649bdb27e28f2be2a0e19156d2b02a3e8c0b9b
Carson Evans <carsonwevans@gmail.com>
2025-07-12 19:23:49 -0400

    added help text

--- src/cli.nim
+++ src/cli.nim
@@ -2,6 +2,21 @@ import parseopt
 
 from localization import supportedLanguages
 
+proc printHelp() =
+    echo """Usage: wttrbar [options]
+Options:
+  --main-indicator=<indicator>   Set the main weather indicator (default: temp_C)
+  --custom-indicator=<indicator> Set a custom weather indicator
+  --location=<location>          Specify the location for weather data
+  --vertical-view                Use vertical view mode
+  --ampm                         Use 12-hour format with AM/PM
+  --nerd                         Use nerd icons for weather symbols
+  --fahrenheit                   Use Fahrenheit for temperature
+  --mph                          Use miles per hour for wind speed
+  --lang=<language>              Set the language for output (default: en)
+  --observation-time             Show the observation time of the weather data
+  --help                         Show this help message"""
+
 proc parseArgs*(): tuple = 
     var mainIndicator: string = "temp_C"
     var customIndicator: string = ""
@@ -16,9 +31,12 @@ proc parseArgs*(): tuple =
 
     for kind, key, value in getOpt():
         case kind
-
         of cmdLongOption:
             case key
+            of "help":
+                printHelp()
+                quit(0)
+
             of "main-indicator":
                 mainIndicator = value
 
